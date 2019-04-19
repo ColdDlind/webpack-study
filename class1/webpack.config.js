2ab5ef9ef500a47433042df24dc1afd008595ad0
dengjian <dengjian@qxy37.com>
2019-04-19 10:02:47 +0800

    webpack初试

---
+++ class1/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+module.exports = {
+  mode: "development", //打包模式   production development
+  entry: "./src/index.js", //入口
+  output: {
+    filename: "bundle.js", //打包后的文件
+    path: path.resolve(__dirname, "dist") //路径为绝对路径
+  }
+};
