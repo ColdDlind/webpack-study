263b3fd56dc0dc4e646b9a79cde7e41b57ed99b8
dengjian <dengjian@qxy37.com>
2019-04-19 10:40:56 +0800

    webpack-dev-serve基本配置

---
+++ class2/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+module.exports = {
+  mode: "development", //打包模式   production development
+  entry: "./src/index.js", //入口
+  output: {
+    filename: "bundle.js", //打包后的文件
+    path: path.resolve(__dirname, "build") //路径为绝对路径
+  },
+  devServer: {
+    //webpack-dev-server配置
+    port: 8090,
+    progress: true,
+    contentBase: "./build",
+    open: true,
+    compress: true
+  }
+};
